5a3484f8e7b1627c0e72316955aa3283fcfada20
Rooster <nguyenthoai06011990@gmail.com>
2024-09-27 16:02:41 +0700

    update

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 
 
+docker buildx create --use
+
 docker buildx build --platform linux/amd64 -t roster90/enteral-mini-app-api:0.1.0 --load .
 
 docker buildx build --platform linux/amd64,linux/arm64 -t roster90/enteral-mini-app-api:0.1.0 --push .
